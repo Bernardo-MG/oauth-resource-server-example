5367c10d3b50c8d34de88c00270ac107c75c3e3d
Bernardo-MG <programming@bernardo-mg.com>
2023-02-04 14:05:05 +0100

    Using Lombok logger

--- src/main/java/com/bernardomg/example/oauth/resource/log/AuditEventLogger.java
+++ src/main/java/com/bernardomg/example/oauth/resource/log/AuditEventLogger.java
@@ -26,13 +26,13 @@ package com.bernardomg.example.oauth.resource.log;
 
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.audit.AuditEvent;
 import org.springframework.boot.actuate.audit.listener.AuditApplicationEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Listens for audit events and logs them.
@@ -41,12 +41,8 @@ import org.springframework.stereotype.Component;
  *
  */
 @Component
+@Slf4j
 public class AuditEventLogger {
-
-    /**
-     * Logger for the event listener.
-     */
-    private static final Logger LOGGER = LoggerFactory.getLogger(AuditEventLogger.class);
 
     @EventListener
     public void auditEventHappened(final AuditApplicationEvent auditApplicationEvent) {
@@ -89,7 +85,7 @@ public class AuditEventLogger {
             messageBuilder.append(")");
         }
 
-        LOGGER.debug(messageBuilder.toString());
+        log.debug(messageBuilder.toString());
     }
 
 }
