715becc12d8e0a2b532c130ca28b21796b511c86
Bernardo <programming@bernardomg.com>
2023-09-11 12:24:06 +0200

    Updated dependencies

---
+++ src/main/java/com/bernardomg/example/spring/security/ws/oauth/resource/security/configuration/ScopeJwtAuthenticationConverter.java
@@ -0,0 +1,22 @@
+package com.bernardomg.example.spring.security.ws.oauth.resource.security.configuration;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+
+public final class ScopeJwtAuthenticationConverter extends JwtAuthenticationConverter {
+
+    public ScopeJwtAuthenticationConverter() {
+        super();
+
+        final JwtGrantedAuthoritiesConverter authoritiesConverter;
+        
+        authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        // Remove scope_ Prefix of
+        authoritiesConverter.setAuthorityPrefix("");
+        // Obtain permission from the field in JWT claim, and the mode is
+        // obtained from the scope or SCP field
+        authoritiesConverter.setAuthoritiesClaimName("scope");
+        setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+    }
+    
+}
